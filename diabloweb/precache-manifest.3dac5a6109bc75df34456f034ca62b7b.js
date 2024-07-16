e527aae37f13b372a348a85fb95a591d7a84e6a4
sheyi_huawei <1912070@qq.com>
2024-07-16 16:53:28 +0800

    add 暗黑1

---
+++ diabloweb/precache-manifest.3dac5a6109bc75df34456f034ca62b7b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71ba5fb375752153c9bc4a1c34faaa99",
+    "url": "/diabloweb/8acc76fdb6ee253c485e.worker.js"
+  },
+  {
+    "revision": "4115803155f6ffac29dc0498aae8c82f",
+    "url": "/diabloweb/d2271be9a67638d3642f.worker.js"
+  },
+  {
+    "revision": "20661cdca14f65e7f2b74f7739be9cc2",
+    "url": "/diabloweb/index.html"
+  },
+  {
+    "revision": "2ea328faa56b6a4bd092",
+    "url": "/diabloweb/static/css/main.e1dd4272.chunk.css"
+  },
+  {
+    "revision": "ced5f643588b32106eeb",
+    "url": "/diabloweb/static/js/0.aaa06a1e.chunk.js"
+  },
+  {
+    "revision": "8e5f4e90fa9e6eaf2fc7",
+    "url": "/diabloweb/static/js/5.430d8c5c.chunk.js"
+  },
+  {
+    "revision": "2ea328faa56b6a4bd092",
+    "url": "/diabloweb/static/js/main.0a18bc0c.chunk.js"
+  },
+  {
+    "revision": "077fe45e700d7971de0b",
+    "url": "/diabloweb/static/js/runtime~main.bf17ce54.js"
+  },
+  {
+    "revision": "4bb3ed3f7b20a9072773",
+    "url": "/diabloweb/static/js/runtime~storage.83a78cef.js"
+  },
+  {
+    "revision": "8e36c4353450853391cd",
+    "url": "/diabloweb/static/js/storage.8fcdd212.chunk.js"
+  },
+  {
+    "revision": "570bd59ab481c6418fcd26ed12f9da86",
+    "url": "/diabloweb/static/media/Diablo.570bd59a.wasm"
+  },
+  {
+    "revision": "9c2373b2a0f03f2726973135ecac793c",
+    "url": "/diabloweb/static/media/DiabloSpawn.9c2373b2.wasm"
+  },
+  {
+    "revision": "b370797d4bcf68aaaf61bee18dc0d316",
+    "url": "/diabloweb/static/media/ListFile.b370797d.txt"
+  },
+  {
+    "revision": "8aa3c6dc817dba93c55eb71ff6e60c20",
+    "url": "/diabloweb/static/media/MpqCmp.8aa3c6dc.wasm"
+  },
+  {
+    "revision": "ddc5d817712fdde5a23034ba0b719c78",
+    "url": "/diabloweb/static/media/move.ddc5d817.svg"
+  },
+  {
+    "revision": "bd1e6e6341fd8472679cd9774f8e25b0",
+    "url": "/diabloweb/static/media/rmb.bd1e6e63.svg"
+  },
+  {
+    "revision": "b40e4a63264bd9ddb182072a2fc6b25d",
+    "url": "/diabloweb/static/media/shift.b40e4a63.svg"
+  },
+  {
+    "revision": "469777abc7b73bbcc138f5508946c162",
+    "url": "/diabloweb/storage.html"
+  }
+]);
